91dd3f686ea6f6478e66c1740197b46352b183e6
‪Ahmed Hamada‬‏ <63430772+ahmedhamada20@users.noreply.github.com>
2022-11-14 23:01:59 +0200

    edit

--- resources/views/website/index.blade.php
+++ resources/views/website/index.blade.php
@@ -268,7 +268,7 @@
                   <img src="{{ asset($row->image) }}" alt="Hand touching basil leaves" width="700px" height="700px"
                   class="mw-100" />
                   @else
-                  <img src="{{ asset('admin/pictures/subcategory/'.rand(1,6).'/'.rand(1,6).".jpg") }}" alt="Hand touching basil leaves" width="50px" height="10px"
+                  <img src="{{ asset('admin/pictures/subcategory/'.rand(1,6).'/'.rand(1,6).".jpg") }}" alt="Hand touching basil leaves" width="700px" height="700px"
                   class="mw-100" />
                   @endif
                   
