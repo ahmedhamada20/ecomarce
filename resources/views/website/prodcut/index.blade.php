504e593f42314f9d34b56d9255bc3eadb9c561fa
‪Ahmed Hamada‬‏ <63430772+ahmedhamada20@users.noreply.github.com>
2022-11-19 12:43:54 +0200

    edit

--- resources/views/website/prodcut/index.blade.php
+++ resources/views/website/prodcut/index.blade.php
@@ -127,7 +127,7 @@ PRODUCT
 
                                 <div data-slide="0" class='carousel-item active'>
                                     @foreach ($data->photos as $row)
-                                    @if ($loop->last)
+                                    @if ($loop->latest)
                                     <img src="{{ asset('admin/pictures/product/'.$data->id.'/'.$data->photo->Filename) }}"  width="700px" height="700px" class="d-block img-fluid"
                                     alt="Salanova&reg; Hydroponic Red Butter Salanova&reg; Lettuce"
                                     itemprop="image"/>
