a8e2a30f50a027bb1ae86ff9d096c4d03e89c876
‪Ahmed Hamada‬‏ <63430772+ahmedhamada20@users.noreply.github.com>
2022-11-20 12:14:22 +0200

    edit

--- resources/views/website/index.blade.php
+++ resources/views/website/index.blade.php
@@ -317,7 +317,7 @@
                 <a
                   href="{{ route('minproduct',$row->id) }}">
                   {{-- @if ($row->image) --}}
-                  <img src="{{ asset($row->image) }}" alt="Hand touching basil leaves" width="700px" height="700px"
+                  <img src="{{ asset($row->image) }}" alt="Hand touching basil leaves" width="272px" height="272px"
                   class="mw-100" />
                   {{-- @else
                   <img src="{{ asset('admin/pictures/subcategory/'.rand(1,6).'/'.rand(1,6).".jpg") }}" alt="Hand touching basil leaves" width="700px" height="700px"
